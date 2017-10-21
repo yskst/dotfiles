c3c5fdd89411ba0f0103aba0efe52cf08c18fdaa
yskst <you@yosukes-Mac-Book.local>
2017-10-22 01:19:35 +0900

    bug fix

--- files/bashrc.d/peco.bash
+++ files/bashrc.d/peco.bash
@@ -12,11 +12,11 @@ function peco-history() {
     READLINE_LINE=$l
     READLINE_POINT=${#l}
 }
-bind -x '"C-r": peco-history'
+bind -x '"\C-r": peco-history'
 
 function peco-ssh() {
     local h=`cut -d' ' -f 1 $HOME/.ssh/known_hosts | $tac | peco`
     READLINE_LINE="ssh $l"
     READLINE_POINT=${#READLINE_POINT}
 }
-bind -x '"C-s": peco-ssh'
+bind -x '"\C-s": peco-ssh'
