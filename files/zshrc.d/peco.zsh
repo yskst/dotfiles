dadbd197b048a14944c3cda2506c4cddaac48129
yskst <you@yosukes-Mac-Book.local>
2017-10-28 00:10:30 +0900

    bug fix

--- files/zshrc.d/peco.zsh
+++ files/zshrc.d/peco.zsh
@@ -8,7 +8,7 @@ if [ `uname` = 'Darwin' ];then
 fi
 
 function peco-history() {
-    local BUFFER=`history -n 1 | eval $tac | peco`
+    local BUFFER="`history -n 1 | eval $tac | peco`"
     local CURSOR=$#BUFFER
     zle reset-prompt
 }
@@ -16,7 +16,7 @@ zle -N peco-history
 bindkey '^R' peco-history
 
 function peco-ssh() {
-    local h=`cut -d' ' -f1 $HOME/.ssh/known_hosts | eval $tac | tr ',' '\n'| peco --prompt 'ssh> '`
+    local h="`cut -d' ' -f1 $HOME/.ssh/known_hosts | eval $tac | tr ',' '\n'| peco --prompt 'ssh> '`"
     if [ -n $h ];then
         BUFFER="ssh $h"
     fi
