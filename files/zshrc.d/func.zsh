149767402f6f2025054a15f7f01d29fb5757010c
yskst <you@yosukes-Mac-Book.local>
2017-12-17 14:00:37 +0900

    add gcd command

--- files/zshrc.d/func.zsh
+++ files/zshrc.d/func.zsh
@@ -3,3 +3,10 @@ function ssh() {
     echo -ne "\ek$host\e\\"
     command ssh $@
 }
+
+function gcd() {
+    if ! git rev-parse --is-inside-work-tree >/dev/null; then
+        return 1
+    fi
+    cd `git rev-parse --show-cdup`$1
+}
